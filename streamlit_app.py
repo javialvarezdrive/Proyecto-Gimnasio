99d2c13881875c5755fd28b1e92eaa1d55a19b78
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 20:51:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from modules import auth, members, schedule, reports
 
 def init_supabase():
     # Reemplaza con tus credenciales de Supabase
-    SUPABASE_URL = "TU_SUPABASE_URL"
-    SUPABASE_KEY = "TU_SUPABASE_KEY"
+    SUPABASE_URL = "https://anqvjvjpcokkwspaecfc.supabase.co"
+    SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFucXZqdmpwY29ra3dzcGFlY2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1OTMyMDcsImV4cCI6MjA1NjE2OTIwN30.w4Y6sE8UyIA22pt5QAYQlcsWZceksF4AKF0zm7Jv7Lk"
     return create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def login_page():
