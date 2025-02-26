1d4fe2c6f5618c9eb5ca0b3dd833a188b353b2e5
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 18:23:35 +0100

    Update members.py

--- modules/members.py
+++ modules/members.py
@@ -1,1 +1,22 @@
+import streamlit as st
 
+def register_member(nip: str, nombre: str, apellidos: str, seccion: str, grupo: str):
+    """
+    Registra un nuevo miembro en la base de datos.
+    """
+    supabase = st.session_state.supabase_client
+    try:
+        nip_int = int(nip)
+    except ValueError:
+        st.error("El NIP debe ser numérico.")
+        return None
+
+    data = {
+      "nip": nip_int,
+      "nombre": nombre,
+      "apellidos": apellidos,
+      "seccion": seccion,
+      "grupo": grupo
+    }
+    response = supabase.table("gym_members").insert(data).execute()
+    return response
