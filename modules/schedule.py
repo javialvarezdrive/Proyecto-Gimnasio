4c61d60647b9cbb1ed463533a9d5ff4ddd49a64e
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 19:25:20 +0100

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def schedule_activity(member_id: int, activity_id: int, fecha: str, turno: str, monitor_id: int):
     """
-    Agenda una actividad para un miembro, registrando la fecha, turno, y el monitor que agendó.
+    Agenda una actividad determinada para un miembro.
     """
-    supabase = st.session_state.supabase_client
+    supabase = st.session_state.get("supabase_client")
     data = {
         "member_id": member_id,
         "activity_id": activity_id,
